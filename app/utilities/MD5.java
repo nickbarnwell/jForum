de6f7b386ae8d4478da4ad9950f149389c254ce0
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-01-12 00:20:38 +0100

    Minor renames for legibility purposes

--- app/utilities/MD5.java
+++ app/utilities/MD5.java
@@ -6,7 +6,7 @@ import java.security.NoSuchAlgorithmException;
  
 public class MD5 { 
  
-    private static String convertToHex(byte[] data) { 
+    private static String convertToHexadecimal(byte[] data) { 
         StringBuffer buffer = new StringBuffer();
         for (int character = 0; character < data.length; character++) { 
             int halfbyte = (data[character] >>> 4) & 0x0F;
@@ -29,7 +29,7 @@ public class MD5 {
         byte[] md5hash = new byte[32];
         md.update(text.getBytes("iso-8859-1"), 0, text.length());
         md5hash = md.digest();
-        return convertToHex(md5hash);
+        return convertToHexadecimal(md5hash);
     } 
 } 
  
