0d8b08915bee109b079cc11433cbf67df8e7094f
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-03-09 21:18:04 +0100

    Updated jobs to reset answers and questions to false if null. Future questions should be false by default

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -3,16 +3,34 @@ import java.util.List;
 import play.*;
 import play.jobs.*;
 import play.test.*;
-
 import models.*;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
+	public void doJob() {
+		if (play.Play.id == "prod") {
+			if (User.find("isAdmin is True").fetch().size() <= 0) {
+				Fixtures.load("admin.yml");
+			}
+		}
+		resetMails();
+	}
 
-    public void doJob() {
-    	System.out.print(User.find("isAdmin is True").fetch().size());
-    	if(User.find("isAdmin is True").fetch().size()==0) {
-    		Fixtures.load("admin.yml");;
-    	}
-    } 
+	public void resetMails() {
+		List<Question> qs = Question.findAll();
+		for (Question q : qs) {
+			if (q.mailed == null) {
+				q.mailed = false;
+				q.save();
+
+			}
+		}
+		List<Answer> as = Answer.findAll();
+		for (Answer a : as) {
+			if (a.mailed == null) {
+				a.mailed = false;
+				a.save();
+			}
+		}
+	}
 }
