09184ece97629327374d82e56d9a1262ccecf6d0
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-03-22 12:36:39 +0100

    updated mailer

--- app/notifiers/Mails.java
+++ app/notifiers/Mails.java
@@ -7,33 +7,35 @@ import play.mvc.*;
 import java.util.*;
 
 public class Mails extends Mailer{
+	private final static String from = "Labyrinth Staff <labyrinth@boltoncomputing.com>";
+	private final static String replyTo = "labyrinth@boltoncomputing.com";
     public static void notifyQuestion(Question question) {
-        setFrom("Nick Barnwell <nick.barnwell@gmail.com>");
-        setReplyTo("nick.barnwell@gmail.com");
+        setFrom(from);
+        setReplyTo(replyTo);
         setSubject("%s, you've been asked a question!", question.receiver);
         addRecipient(question.receiver.email);
         send(question);   
     }
     
     public static void approveQuestion(Question question) {
-        setFrom("Nick Barnwell <nick.barnwell@gmail.com>");
-        setReplyTo("nick.barnwell@gmail.com");
+        setFrom(from);
+        setReplyTo(replyTo);
         setSubject("%s, you need to approve your Forum question!", question.author, question.receiver);
         addRecipient(question.author.email);
         send(question);   
     }
     
     public static void approveAnswer(Answer answer) {
-        setFrom("Nick Barnwell <nick.barnwell@gmail.com>");
-        setReplyTo("nick.barnwell@gmail.com");
+        setFrom(from);
+        setReplyTo(replyTo);
         setSubject("%s, you need to approve your Forum answer!", answer.author );
         addRecipient(answer.author.email);
         send(answer);   
     }
     
     public static void notifyRegistration(User user) {
-    	setFrom("Nick Barnwell <nick.barnwell@gmail.com>");
-        setReplyTo("nick.barnwell@gmail.com");
+    	setFrom(from);
+        setReplyTo(replyTo);
         
         List<User> admins = User.find("isAdmin is true").fetch();
         System.out.println("Is this thing on?");
